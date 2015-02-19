9a4accb80c91ac6dcbbba5b4bf574bc772192c5d
Shoichi Kaji <skaji@cpan.org>
2015-02-19 23:20:13 +0900

    read 1MB

--- lib/Shared/Hash/File.pm
+++ lib/Shared/Hash/File.pm
@@ -4,6 +4,7 @@ use warnings;
 use Fcntl qw(:DEFAULT :flock);
 use File::Temp qw(tempfile);
 use Storable qw(nfreeze thaw);
+use constant CHUNK_SIXE => 1024**2;
 
 {
     package Shared::Hash::File::Guard;
@@ -50,7 +51,7 @@ sub _slurp {
     my $fh = $self->fh;
     sysseek $fh, 0, 0;
     my $buffer = "";
-    while (sysread $fh, my $buf, 8024) {
+    while (sysread $fh, my $buf, CHUNK_SIXE) {
         $buffer .= $buf;
     }
     thaw $buffer;
